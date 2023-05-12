6239d7c4185eb1ee29780d360c21cc136965545f
박경륜 <parkrye@gmail.com>
2023-05-12 21:11:06 +0900

    feat: Homework
    2023-05-12

---
+++ Assets/Homework/2023-05-12/BallMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BallMovement : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [SerializeField] Vector3 dir;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        rb.AddForce(dir, ForceMode.Force);
+    }
+
+    void OnMove(InputValue inputValue)
+    {
+        Debug.Log("ball : " + inputValue.Get<Vector2>());
+        dir.x = inputValue.Get<Vector2>().x;
+        dir.z = inputValue.Get<Vector2>().y;
+    }
+
+    void OnFire(InputValue inputValue)
+    {
+        rb.AddForce(Vector3.up * 200f, ForceMode.Force);
+    }
+}
---
+++ Assets/Homework/2023-05-12/TankMovement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TankMovement : MonoBehaviour
+{
+    [SerializeField] Vector3 dir;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(transform.position + transform.up, transform.forward * 5f + transform.up, Color.red);
+        Move();
+        Turn();
+    }
+
+    void Move()
+    {
+        transform.Translate(transform.forward * dir.z * 0.01f);
+    }
+
+    void Turn()
+    {
+        transform.Rotate(transform.up * dir.x);
+    }
+
+    void OnLook(InputValue inputValue)
+    {
+        dir.x = inputValue.Get<Vector2>().x;
+        dir.z = inputValue.Get<Vector2>().y;
+    }
+}
